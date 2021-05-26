36b6b2fdafed324a5a1547ce477a3cfa56d55738
Ramon Iñiguez <riniguez91@live.com>
2021-05-26 19:33:14 +0200

    Fixed spinner only firing once even though there were multiple HTTP calls

--- src/app/interceptors/spinner.interceptor.ts
+++ src/app/interceptors/spinner.interceptor.ts
@@ -12,14 +12,35 @@ import { finalize } from 'rxjs/operators';
 @Injectable()
 export class SpinnerInterceptor implements HttpInterceptor {
 
+  requestCount = 0;
+
   constructor(private spinnerService: SpinnerService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log(request);
-    this.spinnerService.show();
+    this.startRequest()
 
     return next.handle(request).pipe(
-      finalize( () => this.spinnerService.hide())
+      finalize( () => this.endRequest())
     );
+  }
+
+  private startRequest(): void {
+    // If this is the first request,start the spinner
+    if (this.requestCount == 0) 
+      this.spinnerService.show();
+
+    this.requestCount++;
+  }
+
+  private endRequest(): void {
+    // No request ongoing, so make sure we don’t go to negative count.
+    // Should never happen, but it is better to be safe than sorry.
+    if (this.requestCount == 0)
+        return;
+
+    this.requestCount--;
+    // If it was the last request, call the service to stop the spinner.
+    if (this.requestCount == 0)
+      this.spinnerService.hide();
   }
 }
